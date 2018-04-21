0e14902eac6c6e4a0464788f6b46f34ba38330bd
AbdulkadirKaraca <kadirkaraca1993@gmail.com>
2018-04-21 17:11:28 +0300

    https://github.com/AbdulkadirKaraca/Deneme.git

---
+++ Personel/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Personel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtDTarihi = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.txtSehir = new System.Windows.Forms.TextBox();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.btnIptal = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.cbNo = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.resimBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No\'ya Göre Ara";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Soyad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Doğum Tarihi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Adres";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Şehir";
+            // 
+            // txtAd
+            // 
+            this.txtAd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAd.Location = new System.Drawing.Point(118, 64);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(121, 20);
+            this.txtAd.TabIndex = 6;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSoyad.Location = new System.Drawing.Point(118, 100);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(121, 20);
+            this.txtSoyad.TabIndex = 7;
+            // 
+            // txtDTarihi
+            // 
+            this.txtDTarihi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDTarihi.Location = new System.Drawing.Point(118, 137);
+            this.txtDTarihi.Name = "txtDTarihi";
+            this.txtDTarihi.Size = new System.Drawing.Size(121, 20);
+            this.txtDTarihi.TabIndex = 8;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAdres.Location = new System.Drawing.Point(118, 162);
+            this.txtAdres.Multiline = true;
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAdres.Size = new System.Drawing.Size(206, 86);
+            this.txtAdres.TabIndex = 9;
+            // 
+            // txtSehir
+            // 
+            this.txtSehir.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSehir.Location = new System.Drawing.Point(118, 268);
+            this.txtSehir.Name = "txtSehir";
+            this.txtSehir.Size = new System.Drawing.Size(131, 20);
+            this.txtSehir.TabIndex = 10;
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.Location = new System.Drawing.Point(57, 314);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(75, 23);
+            this.btnYeni.TabIndex = 11;
+            this.btnYeni.Text = "&Yeni";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            this.btnYeni.Click += new System.EventHandler(this.btnYeni_Click);
+            // 
+            // btnIptal
+            // 
+            this.btnIptal.Location = new System.Drawing.Point(160, 314);
+            this.btnIptal.Name = "btnIptal";
+            this.btnIptal.Size = new System.Drawing.Size(75, 23);
+            this.btnIptal.TabIndex = 12;
+            this.btnIptal.Text = "İ&ptal";
+            this.btnIptal.UseVisualStyleBackColor = true;
+            this.btnIptal.Click += new System.EventHandler(this.btnIptal_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(263, 314);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 13;
+            this.btnKaydet.Text = "&Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(371, 314);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 14;
+            this.btnSil.Text = "&Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // cbNo
+            // 
+            this.cbNo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbNo.FormattingEnabled = true;
+            this.cbNo.Location = new System.Drawing.Point(118, 33);
+            this.cbNo.Name = "cbNo";
+            this.cbNo.Size = new System.Drawing.Size(121, 21);
+            this.cbNo.TabIndex = 15;
+            this.cbNo.SelectedIndexChanged += new System.EventHandler(this.cbNo_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(371, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 142);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // resimBox
+            // 
+            this.resimBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resimBox.Location = new System.Drawing.Point(371, 200);
+            this.resimBox.Name = "resimBox";
+            this.resimBox.Size = new System.Drawing.Size(137, 20);
+            this.resimBox.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(371, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Resim Adı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 349);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.resimBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cbNo);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnIptal);
+            this.Controls.Add(this.btnYeni);
+            this.Controls.Add(this.txtSehir);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.txtDTarihi);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtDTarihi;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.TextBox txtSehir;
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.Button btnIptal;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.ComboBox cbNo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox resimBox;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ resimsiz olan/Personel/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Personel
+{
+    public partial class Form1 : Form
+    { 
+        OleDbConnection conn= new OleDbConnection ("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=kisi.mdb");
+
+        public void Kaydet()
+        {
+            OleDbCommand kaydet = new OleDbCommand("INSERT INTO Personel(Ad,Soyad,DogumTarihi,Adres,Sehir) VALUES ('"
+                + txtAd.Text + "','" + txtSoyad.Text + "','" + txtDTarihi.Text + "','" + txtAdres.Text + "','" + txtSehir.Text + "')", conn);
+
+            try
+            {
+                conn.Open();
+                kaydet.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally 
+            { 
+                conn.Close(); 
+            } 
+        } 
+     public void Sil(int ID) 
+     {
+         OleDbCommand sil = new OleDbCommand("DELETE  FROM Personel WHERE Numara=" + ID + "", conn);
+
+
+   try 
+   { 
+    conn.Open(); 
+    sil.ExecuteNonQuery(); 
+   } 
+   catch(Exception ex) 
+   { 
+     MessageBox.Show(ex.Message); 
+    } 
+   finally 
+   { 
+    conn.Close(); 
+   } 
+  }
+     public void IDDoldur()
+     {
+         cbNo.Items.Clear();
+
+         OleDbCommand veri = new OleDbCommand("SELECT Numara FROM Personel ORDER BY Numara", conn);
+         OleDbDataReader oku = null;
+         try 
+         { 
+             conn.Open(); 
+             oku=veri.ExecuteReader(); 
+             while(oku.Read())
+             { 
+                 cbNo.Items.Add(oku.GetInt32(0)); 
+             }
+         }
+         catch (Exception ex)
+         {
+             MessageBox.Show(ex.Message);
+         }
+         finally
+         {
+             oku.Close();
+             conn.Close();
+         } 
+     }
+
+     public void IDyeGoreFormDoldur(int ID)
+     {
+         OleDbCommand veri = new OleDbCommand("SELECT Numara, Ad, Soyad, DogumTarihi, Adres, Sehir FROM Personel where Numara="+ID+"", conn);
+         OleDbDataReader oku=null;
+         try
+         {
+             conn.Open();
+             oku=veri.ExecuteReader();
+             if (oku.Read())
+             {
+                 txtAd.Text = oku["Ad"].ToString();
+                 txtSoyad.Text = oku["Soyad"].ToString();
+                 txtAdres.Text = oku["Adres"].ToString();
+                 txtSehir.Text = oku["Sehir"].ToString();
+                 txtDTarihi.Text = oku["DogumTarihi"].ToString();
+
+             }
+         }
+         catch (Exception ex)
+         {
+             MessageBox.Show(ex.Message);
+         }
+         finally
+         {
+             oku.Close();
+             conn.Close();
+         } 
+     }
+
+     public void Temizle()
+     {
+         txtAd.Text = ""; txtSoyad.Text = "";
+         txtAdres.Text = ""; txtSehir.Text = ""; txtDTarihi.Text = "";
+         txtAd.Focus(); 
+
+     }
+
+     public bool Kontrol()
+     {
+         if (txtAd.Text == "")
+         {
+             MessageBox.Show("Adı Giriniz");
+             txtAd.Focus();
+             return false;
+         }
+         else if (txtSoyad.Text == "")
+         {
+             MessageBox.Show("Soyadı Giriniz");
+             txtSoyad.Focus();
+             return false;
+         }
+         else if (txtDTarihi.Text== "")
+         {
+
+             MessageBox.Show("Doğum Tarihini Giriniz");
+             txtDTarihi.Focus();
+             return false;
+         }
+
+         else if (txtAdres.Text == "")
+         {
+             MessageBox.Show("Adresi Giriniz");
+             txtAdres.Focus();
+             return false;
+         }
+         else if (txtSehir.Text == "")
+         {
+             MessageBox.Show("Şehiri Giriniz");
+             txtSehir.Focus();
+             return false;
+         }
+         else
+         {
+             return true;
+         }
+     } 
+ 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+                if(Kontrol() == true)
+                {
+                    Kaydet();
+                    btnYeni.Enabled = true;
+                    btnKaydet.Enabled = false;
+                    btnIptal.Enabled = false;
+                    IDDoldur();
+                    cbNo.SelectedIndex = cbNo.Items.Count - 1;
+                } 
+        }
+
+        private void btnYeni_Click(object sender, EventArgs e)
+        {
+            Temizle();
+            btnYeni.Enabled = false;
+            btnKaydet.Enabled = true;
+            btnIptal.Enabled = true;
+            cbNo.SelectedIndex = -1; 
+        }
+
+        private void btnIptal_Click(object sender, EventArgs e)
+        {
+
+            Temizle();
+            btnYeni.Enabled = true;
+            btnKaydet.Enabled = false;
+            btnIptal.Enabled = false;
+            cbNo.SelectedIndex = 0; 
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+         if(MessageBox.Show(cbNo.SelectedItem + " nolu kaydı silmek istiyor musunuz?", this.Text, 
+MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes) 
+    { 
+       Sil(Convert.ToInt32(cbNo.SelectedItem)); 
+       IDDoldur(); 
+       cbNo.SelectedIndex = cbNo.Items.Count - 1; 
+    } 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IDDoldur();
+            cbNo.SelectedIndex = 0; 
+        }
+
+        private void cbNo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            IDyeGoreFormDoldur(Convert.ToInt32(cbNo.SelectedItem)); 
+        }
+
+        
+
+
+       
+    }
+}
